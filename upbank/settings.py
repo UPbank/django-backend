ce5f582ed1a11b2fa01f94cd658e245d0e230e83
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-10-02 16:54:36 +0100

    Added cors headers

--- upbank/settings.py
+++ upbank/settings.py
@@ -50,11 +50,13 @@ INSTALLED_APPS = [
 	# Outside apps
 	'rest_framework',
 	'knox',
+	'corsheaders',
 ]
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
