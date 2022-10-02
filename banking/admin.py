2366a7f3ee0a15a3b472662877abb4f9a0156109
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-10-02 16:40:09 +0100

    Changed import

--- banking/admin.py
+++ banking/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import *
+from . import models
 
-admin.site.register(Account)
-admin.site.register(Address)
-admin.site.register(TelcoProvider)
-admin.site.register(Transfer)
-admin.site.register(Card)
-admin.site.register(DirectDebit)
-admin.site.register(StandingOrder)
+admin.site.register(models.Account)
+admin.site.register(models.Address)
+admin.site.register(models.TelcoProvider)
+admin.site.register(models.Transfer)
+admin.site.register(models.Card)
+admin.site.register(models.DirectDebit)
+admin.site.register(models.StandingOrder)
